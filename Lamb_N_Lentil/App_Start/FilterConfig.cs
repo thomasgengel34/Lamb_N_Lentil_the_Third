cdf46131539cb934199a36a481a69a4f4d4821aa
Tom Engel <thomasgengel34@yahoo.com>
2018-03-13 09:57:47 -0500

    Add project files.

---
+++ Lamb_N_Lentil/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Lamb_N_Lentil
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Lamb_N_Lentil/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lamb_N_Lentil.MvcApplication" Language="C#" %>
